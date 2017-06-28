256d143bc3984d53bfa4b1eeea6fcde6b8eeae37
PNPtutorials <parvinderyadav95@gmail.com>
2017-06-28 21:53:55 +0530

    Update Start-ARM.py

--- Start-ARM.py
+++ Start-ARM.py
@@ -1,15 +1,16 @@
 import time
 import subprocess
  
-subprocess.call (["monstart"])
+subprocess.call (["sudo","insmod","/root/brcmfmac.ko"])
+subprocess.call (["sudo", "ifconfig","wlan0","up"])
 
-process = subprocess.Popen(["airodump-ng", "wlan0", "--write", "pnp", "--output-format", "csv"])
+process = subprocess.Popen(["sudo", "airodump-ng", "wlan0", "--write", "pnp", "--output-format", "csv"])
 time.sleep(30)
 process.terminate()
 
-process = subprocess.Popen(["airdrop-ng", "-i", "wlan0", "-t", "pnp-01.csv", "-r", "rulefile.txt"])
+process = subprocess.Popen(["sudo", "airdrop-ng", "-i", "wlan0", "-t", "pnp-01.csv", "-r", "rulefile.txt"])
 time.sleep(11600)
 process.terminate()
 
-subprocess.call(["rm" ,  "*.csv"])
+subprocess.call(["sudo", "rm" ,  "*.csv"])
 
